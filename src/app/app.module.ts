f9847e0f0dd675b7c510943b50a921748761f25d
Michel Vidailhet <michel.vidailhet@gmail.com>
2021-05-11 12:12:25 +0200

    Création des composants pokemon-list et pokemon-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PokemonItemComponent } from './components/pokemon-item/pokemon-item.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonItemComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule,
