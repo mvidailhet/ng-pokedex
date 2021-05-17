582b33a40aff8150a4dec24394f1d47e2efa019b
Michel Vidailhet <michel.vidailhet@gmail.com>
2021-05-17 15:21:45 +0200

    Utilisation du renderer pour changer le style de l'élément

--- src/app/directives/basic-highlight.directive.ts
+++ src/app/directives/basic-highlight.directive.ts
@@ -1,16 +1,15 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlight]'
 })
 export class BasicHighlightDirective implements OnInit {
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    this.elementRef.nativeElement.style.backgroundColor = 'green';
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
   }
-
 
 
 }
