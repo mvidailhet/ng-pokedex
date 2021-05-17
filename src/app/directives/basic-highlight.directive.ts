5f7833c10b24255d2f2ec15471d2febf55f59d37
Michel Vidailhet <michel.vidailhet@gmail.com>
2021-05-17 15:23:35 +0200

    Utilisation de HostListener pour écouter les évènements de l'élément

--- src/app/directives/basic-highlight.directive.ts
+++ src/app/directives/basic-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlight]'
@@ -8,8 +8,13 @@ export class BasicHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('mouseenter') mouseEnter(event: Event) {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
   }
 
-
+  @HostListener('mouseleave') mouseLeave(event: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+  }
 }
