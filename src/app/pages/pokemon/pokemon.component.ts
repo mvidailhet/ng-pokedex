3eb426af1e0ec037cf3bfcc8f509d7229a11a203
Michel Vidailhet <michel.vidailhet@gmail.com>
2021-05-18 12:23:46 +0200

    Destruction de l'observable des params. Ce n'est pas nécessaire, mais on le fait pour l'exemple et pourquoi c'est important.

--- src/app/pages/pokemon/pokemon.component.ts
+++ src/app/pages/pokemon/pokemon.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Pokemon, PokemonsService } from 'src/app/services/pokemons.service';
 
 @Component({
@@ -7,13 +8,21 @@ import { Pokemon, PokemonsService } from 'src/app/services/pokemons.service';
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.scss']
 })
-export class PokemonComponent implements OnInit {
+export class PokemonComponent implements OnInit, OnDestroy {
   pokemon: Pokemon | undefined;
+  paramsSubscription: Subscription | undefined;
 
   constructor(private activatedRoute: ActivatedRoute, private pokemonService: PokemonsService) { }
 
+  ngOnDestroy(): void {
+    // Pas besoin de faire ça, Angular s'en occupe pour nous.
+    // Mais pour l'exemple, on montre comment faire
+    // Si cela n'est pas fait, la subscription reste dans la mémoire et crée une fuite mémoire
+    this.paramsSubscription?.unsubscribe();
+  }
+
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: Params) => {
+    this.paramsSubscription = this.activatedRoute.params.subscribe((params: Params) => {
       const pokemonId = params["id"];
       this.pokemon = {
         id: pokemonId,
