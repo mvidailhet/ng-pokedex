bfc23ab53f01dc36ca22765babacc8e204e77b08
Michel Vidailhet <michel.vidailhet@gmail.com>
2021-06-18 15:50:48 +0200

    Ajout d'un paramètre pour définir le nombre de caractères à garder

--- src/app/pipes/shorten.pipe.ts
+++ src/app/pipes/shorten.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ShortenPipe implements PipeTransform {
 
-  transform(value: string | undefined): unknown {
-    return value && value.substr(0, 3) + "...";
+  transform(value: string | undefined, nbCaractersToRemove?: number): unknown {
+    return value && value.substr(0, nbCaractersToRemove || 3) + "...";
   }
 
 }
