d099ab846697e10bd00e1e13b8ed0c0eea7c20c5
Michel Vidailhet <michel.vidailhet@gmail.com>
2021-06-25 12:04:00 +0200

    Submit du formulaire et récupération de l'objet

--- src/app/pages/create-user-template-driven/create-user-template-driven.component.ts
+++ src/app/pages/create-user-template-driven/create-user-template-driven.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user-template-driven',
@@ -10,6 +11,10 @@ export class CreateUserTemplateDrivenComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
+  }
+
+  onSubmit(userForm: NgForm): void {
+    console.log(userForm);
   }
 
 }
